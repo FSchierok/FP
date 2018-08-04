96b3c7119001b4af78767454bf50a06fd8f3f4be
Carlo Tasillo <carlo.tasillo@udo.edu>
2018-08-04 13:08:52 +0200

    added NBayes, Neigh, Tree

--- E5b_IceCube/auswertung_Carlo_BinTree.py
+++ E5b_IceCube/auswertung_Carlo_BinTree.py
@@ -39,7 +39,7 @@ from sklearn.feature_selection import SelectKBest
 def f_regression(X,Y):
    import sklearn
    return sklearn.feature_selection.f_regression(X,Y,center=False) #center=True (the default) would not work ("ValueError: center=True only allowed for dense data") but should presumably work in general
-data_X_selected = SelectKBest(score_func=f_regression, k=20).fit_transform(data_X, data_y) # k = 20 da dadurch höchstmöglicher Jaccard-Index von J = 0.98325
+data_X_selected = SelectKBest(score_func=f_regression, k=2).fit_transform(data_X, data_y) # k = 2 testweise
 
 
 # Permutiere die Indizes von data zufällig beim Aufteilen in Trainings- und
@@ -51,13 +51,13 @@ data_train_X = data_X_selected[perm[:-len(data_X_selected) // 10]]
 data_test_X  = data_X_selected[perm[-len(data_X_selected) // 10:]]
 
 
-# Lernen und überprüfen
-from sklearn.naive_bayes import GaussianNB
+# Lernen und überprüfen mit BinTree
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import jaccard_similarity_score
-model = GaussianNB()
-model.fit(data_train_X, data_train_y)
+bintree = DecisionTreeClassifier(random_state=0)
+bintree.fit(data_train_X, data_train_y)
 expected = data_test_y
-predicted = model.predict(data_test_X)
+predicted = bintree.predict(data_test_X)
 print(metrics.classification_report(expected, predicted))
 print(metrics.confusion_matrix(expected, predicted))
 print(jaccard_similarity_score(expected,predicted))
