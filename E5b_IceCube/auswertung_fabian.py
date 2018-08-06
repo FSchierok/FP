63c23436773b2d5346c9be94e65e8440beedf675
Fabian Schierok <fabian.schierok@tu-dortmund.de>
2018-08-06 16:31:11 +0200

    Add some sorting Algo

---
+++ E5b_IceCube/auswertung_fabian.py
@@ -0,0 +1,25 @@
+import sklearn as skl
+import numpy as np
+import pandas as pd
+
+signal = pd.read_csv("data/signal.csv", delimiter=";")
+bg = pd.read_csv("data/background.csv", delimiter=";")
+# Enfernt alle Spalten, die nicht im Background sind, sowie alle mit Weight, MC oder Event im Namen. Alle ungültigen Werte werden durch den Mittelwert ersetzt
+for category in signal:
+    if category not in bg:
+        del signal[category]
+    elif "Weight" in str(category):
+        del signal[category]
+    elif "MC" in str(category):
+        del signal[category]
+    elif "Event" in str(category):
+        del signal[category]
+    else:
+        for value in signal[category]:
+            if np.isnan(value) or np.isinf(value):
+                value = np.mean(signal[category])
+for category in bg:
+    if category not in signal:
+        del bg[category]
+signal.to_csv("data/signal_fabian.csv", sep=";")
+bg.to_csv("data/bg_fabian.csv", sep=";")
